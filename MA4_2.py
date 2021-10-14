9d212cb44fe92f18e6621291c3020b414e2f0c60
olivialynga <92375796+olivialynga@users.noreply.github.com>
2021-10-14 09:26:20 +0200

    Add files via upload

--- MA4_2.py
+++ MA4_2.py
@@ -1,12 +1,36 @@
 #!/usr/bin/env python3
 
 from integer import Integer
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+def fib_py(n):
+    if n<=1:
+        return n
+    else:
+        return (fib_py(n-1) + fib_py(n-2))
+
+length = [x for x in range(25)]
+
+fib_py_time = []
+
+for i in length:
+    ts= time.time()
+    fib_py(i)
+    fib_py_time.append(time.time()-ts)
+
+plt.plot(length, fib_py_time)
+plt.show()
 
 def main():
 	f = Integer(5)
 	print(f.get())
 	f.set(7)
 	print(f.get())
+	f=Integer(n)
+
+	f.fib()
 
 if __name__ == '__main__':
 	main()
