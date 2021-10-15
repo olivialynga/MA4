4f35efc1c731c7f43881ac03cb1171adc9f229a0
olivialynga <92375796+olivialynga@users.noreply.github.com>
2021-10-15 08:32:00 +0200

    Add files via upload

--- integer.py
+++ integer.py
@@ -10,7 +10,7 @@ class Integer(object):
 		lib.Integer_get.restype = ctypes.c_int
 		lib.Integer_set.argtypes = [ctypes.c_void_p,ctypes.c_int]
 		lib.Integer_delete.argtypes = [ctypes.c_void_p]
-		lib.Integer_fib.argtypes = ctypes.c_void_p
+		lib.Integer_fib.argtypes = [ctypes.c_void_p]
 		lib.Integer_fib.restype = ctypes.c_int
 		self.obj = lib.Integer_new(val)
 
