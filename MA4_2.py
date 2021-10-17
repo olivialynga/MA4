664c8fa78791c63030abfaa2cfce56b2eaae7861
olivialynga <92375796+olivialynga@users.noreply.github.com>
2021-10-17 18:57:29 +0200

    Add files via upload

--- MA4_2.py
+++ MA4_2.py
@@ -44,7 +44,6 @@ def main():
     plt.xlabel('n')
     plt.ylabel('sekunder')
     plt.title('tid fib')
-    plt.legend('fib_py(n)', 'f=Integer(n)')
     matplotlib.pyplot.savefig("time.perf_counter.png")
     plt.show()
 
